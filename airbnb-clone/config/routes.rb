1c908bf5046b5c203c8f1c32e50762e15b4ea90f
bromodachi <curaga1@gmail.com>
2016-03-08 07:50:28 -0500

    facebook

--- airbnb-clone/config/routes.rb
+++ airbnb-clone/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'pages/home'
   root 'pages#home'
-  devise_for :users
+  devise_for :users, :path => '',
+             :path_names=> {:sign_in => 'login', :sign_out => "logout", :edit => 'profile' },
+            :controllers=> {:omniauth_callbacks => 'omniauth_callbacks'}
+  resources :users, only: [:show]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
